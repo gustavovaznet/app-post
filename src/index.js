554c708122d4fbfa452de107c0708f203a67b33e
gustavovaznet <98855318+gustavovaznet@users.noreply.github.com>
2022-03-11 09:16:42 -0300

    Create index file

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//INDEX PAGE
+
+//IMPORTING
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/global-styles.css';
+import { Home } from './templates/Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
